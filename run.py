572844360c2e618e77964325ef265994279817f7
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-08-17 15:59:58 +0200

    Running multiple episodes

--- run.py
+++ run.py
@@ -1,22 +1,16 @@
+from tqdm import tqdm
 import gymnasium as gym
 env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode='human')
 
-observation, info = env.reset()
-print(f"Starting observation: {observation}")
+num_episodes = 10
+for episode in tqdm(range(num_episodes), desc="Running episodes"):
+    observation, info = env.reset()
 
-episode_over = False
-total_reward = 0
-
-while not episode_over:
-    action = env.action_space.sample()
-    print(f"Taking action: {action}")
-
-    # Take the action and see what happens
-    observation, reward, terminated, truncated, info = env.step(action)
-    print(f"New observation: {observation}, Reward: {reward}, Terminated: {terminated}, Truncated: {truncated}")
-
-    total_reward += reward
-    episode_over = terminated or truncated
-
-print(f"Episode finished! Total reward: {total_reward}")
+    episode_over = False
+    total_reward = 0
+    while not episode_over:
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
+        episode_over = terminated or truncated
 env.close()
