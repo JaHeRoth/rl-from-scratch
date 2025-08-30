b697c824ab388829a508bd4ef006ebdcab42df93
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-08-30 21:23:55 +0200

    PolicyGradientAgent

--- bandits.py
+++ bandits.py
@@ -6,7 +6,7 @@ from scipy.stats import norm
 
 
 class Agent(ABC):
-    estimated_values: np.ndarray
+    estimates: np.ndarray = np.array([])
 
     @abstractmethod
     def __init__(self):
@@ -45,7 +45,7 @@ def multiarmed_bandit(
 
     print("=" * 40 + name + "=" * 40)
     print(f"True values: {[round(value, 3) for value in true_values]}")
-    print(f"Estimated values: {[round(value, 3) for value in agent.estimated_values]}")
+    print(f"Estimates: {[round(value, 3) for value in agent.estimates]}")
 
     plt.plot(expected_rewards)
     plt.xlabel("Episode")
@@ -76,14 +76,14 @@ lr = 0.01
 # %%
 class NaiveGreedyAgent(Agent):
     def __init__(self, n_arms: int, lr: float):
-        self.estimated_values = np.zeros((n_arms,))
+        self.estimates = np.zeros((n_arms,))
         self.lr = lr
 
     def act(self, random_state):
-        return greedy_action(values=self.estimated_values, random_state=random_state)
+        return greedy_action(values=self.estimates, random_state=random_state)
 
     def update(self, action: int, reward: float):
-        self.estimated_values[action] += self.lr * (reward - self.estimated_values[action])
+        self.estimates[action] += self.lr * (reward - self.estimates[action])
 
 
 multiarmed_bandit(
@@ -96,14 +96,14 @@ multiarmed_bandit(
 # %%
 class OptimisticGreedyAgent(Agent):
     def __init__(self, n_arms: int, lr: float):
-        self.estimated_values = np.zeros((n_arms,)) + norm.ppf(0.99)
+        self.estimates = np.zeros((n_arms,)) + norm.ppf(0.99)
         self.lr = lr
 
     def act(self, random_state):
-        return greedy_action(values=self.estimated_values, random_state=random_state)
+        return greedy_action(values=self.estimates, random_state=random_state)
 
     def update(self, action: int, reward: float):
-        self.estimated_values[action] += self.lr * (reward - self.estimated_values[action])
+        self.estimates[action] += self.lr * (reward - self.estimates[action])
 
 
 multiarmed_bandit(
@@ -116,20 +116,20 @@ multiarmed_bandit(
 # %%
 class EpsGreedyAgent(Agent):
     def __init__(self, n_arms: int, lr: float, eps: float):
-        self.estimated_values = np.zeros((n_arms,))
+        self.estimates = np.zeros((n_arms,))
         self.lr = lr
         self.eps = eps
 
     def act(self, random_state):
         rng = np.random.default_rng(random_state)
         return (
-            greedy_action(values=self.estimated_values, random_state=random_state)
+            greedy_action(values=self.estimates, random_state=random_state)
             if rng.uniform() > self.eps
-            else rng.integers(len(self.estimated_values))
+            else rng.integers(len(self.estimates))
         )
 
     def update(self, action: int, reward: float):
-        self.estimated_values[action] += self.lr * (reward - self.estimated_values[action])
+        self.estimates[action] += self.lr * (reward - self.estimates[action])
 
 
 multiarmed_bandit(
@@ -142,7 +142,7 @@ multiarmed_bandit(
 # %%
 class UCBAgent(Agent):
     def __init__(self, n_arms: int, lr: float, c: float):
-        self.estimated_values = np.zeros((n_arms,))
+        self.estimates = np.zeros((n_arms,))
         self.num_updates = np.zeros((n_arms,))
         self.total_updates = 0
         self.lr = lr
@@ -152,12 +152,12 @@ class UCBAgent(Agent):
         if self.num_updates.min() == 0:
             return np.argmin(self.num_updates)
 
-        ucb_values = self.estimated_values + self.c * np.sqrt(np.log(self.num_updates.sum()) / self.num_updates)
+        ucb_values = self.estimates + self.c * np.sqrt(np.log(self.num_updates.sum()) / self.num_updates)
         return greedy_action(values=ucb_values, random_state=random_state)
 
     def update(self, action: int, reward: float):
         self.num_updates[action] += 1
-        self.estimated_values[action] += self.lr * (reward - self.estimated_values[action])
+        self.estimates[action] += self.lr * (reward - self.estimates[action])
 
 
 multiarmed_bandit(
@@ -165,4 +165,34 @@ multiarmed_bandit(
     seed=seed,
     n_episodes=2 * 10 ** 5,
     agent=UCBAgent(n_arms=n_arms, lr=lr, c=100),
+)
+
+# %%
+class PolicyGradientAgent(Agent):
+    def __init__(self, n_arms: int, lr: float):
+        self.estimates = np.zeros((n_arms,))
+        self.num_updates = 0
+        self.lr = lr
+
+    @staticmethod
+    def _softmax(x: np.ndarray):
+        exponentiated = np.exp(x - x.max())  # Subtraction for numerical stability
+        return exponentiated / exponentiated.sum()
+
+    def act(self, random_state):
+        return np.random.default_rng(random_state).choice(
+            np.arange(len(self.estimates)),
+            p=self._softmax(self.estimates * np.log(self.num_updates + 1)),
+        )
+
+    def update(self, action: int, reward: float):
+        self.num_updates += 1
+        self.estimates[action] += self.lr * (reward - self.estimates[action])
+
+
+multiarmed_bandit(
+    n_arms=n_arms,
+    seed=seed,
+    n_episodes=10 ** 3,
+    agent=PolicyGradientAgent(n_arms=n_arms, lr=lr),
 )
