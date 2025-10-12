f4f0118051a6cf0abd60385fcaebad992e6ba3b9
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-10-12 10:37:14 +0200

    Copied over code

--- tabular_rl_control.py
+++ tabular_rl_control.py
@@ -480,6 +480,114 @@ print(
 print(policy.pivot(on="action", index="state").sort("state"))
 
 # %%
+# Dyna-Q with prioritized sweeping
+# ~10k * 10 needed to reach optimal policy on 4x4
+num_steps = 3_000
+n_samples_per_step = 10
+
+class UniquePriorityQueue:
+    items: list = []
+    priorities: list = []
+
+    # Simple implementation, can probably be made more efficient
+    def put(self, item, priority):
+        if item not in self.items:
+            self.items.append(item)
+            self.priorities.append(priority)
+        elif self.priorities[item_idx := self.items.index(item)] < priority:
+            self.priorities[item_idx] = priority
+
+        order = np.argsort(self.priorities)
+        self.items = list(np.array(self.items)[order])
+        self.priorities = list(np.array(self.priorities)[order])
+
+    def pop(self):
+        self.priorities.pop()
+        return self.items.pop()
+
+    def __len__(self):
+        return len(self.items)
+
+def dyna_q(
+    q_in: pl.DataFrame,
+    gamma: float,
+    eps_schedule: Iterable[float],
+    lr_schedule: Iterable[float],
+    n_samples_per_step: int,
+    seed: int = 42,
+) -> pl.DataFrame:
+    q = (
+        q_in.sort(["state", "action"])
+        .pivot(on="action", index="state")
+        .drop("state")
+        .to_numpy()
+    )
+    # If we rather want every experienced state-action pair to be sampled with equal probability, then this
+    #  `[(state, action, next_state, reward)]` should be replaced by `{(state, action): [(next_state, reward)]`
+    #  and the below single `rng.choice` call should be replaced by two `rng.choice` calls (one to choose the
+    #  state-action pair and the other to choose the experience for that state-action pair)
+    experiences: list[tuple[int, int, int, float]] = []
+
+    rng = np.random.default_rng(seed)
+    state, _ = env.reset(seed=seed)
+    for step, (eps, lr) in tqdm(enumerate(zip(eps_schedule, lr_schedule)), desc="Running steps"):
+        # We only need the behavior policy for the current state
+        state_q = pl.DataFrame(
+            {
+                "state": state,
+                "action": range(q.shape[1]),
+                "q": q[state, :],
+            }
+        )
+        behavior_policy = eps_greedy_policy(q=state_q, eps=eps)
+
+        action = sample_from_policy(policy=behavior_policy, state=state, seed=rng)
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        episode_over = terminated or truncated
+
+        experiences.append((state, action, next_state, reward))
+
+        target = float(reward) + gamma * q[next_state, :].max()
+        q[state, action] += lr * (target - q[state, action])
+
+        for _ in range(n_samples_per_step):
+            _state, _action, _next_state, _reward = rng.choice(experiences)
+            _target = float(_reward) + gamma * q[int(_next_state), :].max()
+            q[int(_state), int(_action)] += lr * (_target - q[int(_state), int(_action)])
+
+        if episode_over:
+            state, _ = env.reset(seed=seed + step)
+        else:
+            state = next_state
+
+    return q_in.with_columns(q=q.flatten())
+
+
+q = dyna_q(
+    q_in=init_q(state_space, action_space),
+    gamma=gamma,
+    eps_schedule=np.linspace(1.0, 0.25, num_steps),
+    lr_schedule=[
+        learning_rate_for_update(
+            base_learning_rate=1e-1,
+            update_number=step,
+            period=100,
+            numerator_power=0.51,
+        )
+        for step in range(num_steps)
+    ],
+    n_samples_per_step=n_samples_per_step,
+)
+policy = eps_greedy_policy(q, eps=0.0)
+print(
+    q.join(policy, on=["state", "action"])
+    .group_by("state")
+    .agg(v=(pl.col("policy") * pl.col("q")).sum())
+    .sort("state")
+)
+print(policy.pivot(on="action", index="state").sort("state"))
+
+# %%
 # Run learned policy
 human_env = gym.make('FrozenLake-v1', desc=None, map_name=map_name, is_slippery=True, render_mode='human')
 
