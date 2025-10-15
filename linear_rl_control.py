cbe2351e933350558c381a896acfe63c4ce87d29
Jacob H. Rothschild <jacob.rothschild@quantco.com>
2025-10-15 07:08:12 +0200

    On-policy control with Sarsa with linear function approximation

--- linear_rl_control.py
+++ linear_rl_control.py
@@ -37,7 +37,7 @@ def featurize(state: np.ndarray, action, action_space: Iterable) -> np.ndarray:
 def init_q_weights(env: Env) -> np.ndarray:
     state = env.observation_space.sample()
     action = env.action_space.sample()
-    featurization = featurize(state, action, env.action_space.n)
+    featurization = featurize(state, action, action_space=range(env.action_space.n))
     return 0.0 * featurization
 
 def sample_from_eps_greedy_policy(
@@ -46,7 +46,7 @@ def sample_from_eps_greedy_policy(
     q = pl.DataFrame(
         {
             "action": action_space,
-            "policy": [
+            "q": [
                 q_weights @ featurize(state, action, action_space)
                 for action in action_space
             ],
@@ -57,9 +57,12 @@ def sample_from_eps_greedy_policy(
         .select(
             "action",
             policy=(
-                    (1 - eps) * pl.col("greedy_choice").cast(pl.Float64)
-                    / pl.sum("greedy_choice").over("state")
-                    + eps / pl.len().over("state")
+                (
+                    (1 - eps)
+                    * pl.col("greedy_choice").cast(pl.Float64)
+                    / pl.sum("greedy_choice")
+                )
+                + eps / pl.len()
             ),
         )
     )
@@ -77,99 +80,104 @@ def learning_rate_for_update(
 def policy_iteration(
     env: Env,
     evaluation_func: callable,
-    gamma: float,
-    lr_schedule: Iterable[float],
     eps_schedule: Iterable[float],
-    required_delta: float,
     verbose: bool,
+    **kwargs,
 ) -> tuple[pl.DataFrame, pl.DataFrame]:
     q_weights = init_q_weights(env)
 
-    for eps in eps_schedule:
-        q_weights_delta_norm = np.inf
-        while q_weights_delta_norm >= required_delta:
-            next_q_weights = evaluation_func(
-                q_weights_in=q_weights, eps=eps, gamma=gamma, lr_schedule=lr_schedule, action_space=env.action_space
-            )
-            q_weights_delta_norm = np.linalg.norm(next_q_weights - q_weights)
-            q_weights = next_q_weights
-            if verbose:
-                print(q_weights)
+    for eps in tqdm(eps_schedule):
+        next_q_weights = evaluation_func(
+            q_weights_in=q_weights, eps=eps, env=env, **kwargs
+        )
+        if verbose:
+            print(f"{eps=:.3f}, q_weights_delta_norm={np.linalg.norm(next_q_weights - q_weights):.3f}")
+        q_weights = next_q_weights
 
-    return q, policy
+    return q_weights
 
 # %%
 # Sarsa
+# Note: We'd probably benefit from some regularization and feature normalization,
+#  but omitted here for brevity and simplicity
+
 def sarsa(
-    q_in: pl.DataFrame, policy: pl.DataFrame, gamma: float, lr_schedule: Iterable[float], seed: int = 42
-) -> pl.DataFrame:
-    q = (
-        q_in.sort(["state", "action"])
-        .pivot(on="action", index="state")
-        .drop("state")
-        .to_numpy()
+    q_weights_in: np.ndarray, eps: float, gamma: float, lr_schedule: Iterable[float], env: Env
+) -> np.ndarray:
+    q_weights = q_weights_in.copy()
+    action_space = range(env.action_space.n)
+
+    state, _ = env.reset()
+    action = sample_from_eps_greedy_policy(
+        state, q_weights_in, action_space, eps
     )
-    state, _ = env.reset(seed=seed)
-    action = sample_from_policy(policy, state, seed=seed)
-    for step, lr in tqdm(enumerate(lr_schedule), desc="Running steps"):
+    for step, lr in enumerate(lr_schedule):
         next_state, reward, terminated, truncated, _ = env.step(action)
         episode_over = terminated or truncated
 
         if episode_over:
             target = float(reward)
         else:
-            next_action = sample_from_policy(policy, state=next_state, seed=seed + step + 1)
-            target = float(reward) + gamma * q[next_state, next_action]
+            next_action = sample_from_eps_greedy_policy(
+                next_state, q_weights_in, action_space, eps
+            )
+            target = (
+                float(reward)
+                + gamma
+                * q_weights @ featurize(
+                    state=next_state, action=next_action, action_space=action_space
+                )
+            )
 
-        q[state, action] += lr * (target - q[state, action])
+        state_action_features = featurize(state, action, action_space)
+        q_weights += lr * (target - q_weights @ state_action_features) * state_action_features
 
         if episode_over:
-            state, _ = env.reset(seed=seed + step)
-            action = sample_from_policy(policy, state, seed=seed + step + 1)
+            state, _ = env.reset()
+            action = sample_from_eps_greedy_policy(
+                state, q_weights_in, action_space, eps
+            )
         else:
             state = next_state
             action = next_action
 
-    return q_in.with_columns(q=q.flatten())
+    return q_weights
 
 
-q, policy = policy_iteration(
-    q_in=init_q(state_space, action_space),
-    policy_in=init_policy(state_space, action_space),
+q_weights = policy_iteration(
+    env=env,
     evaluation_func=sarsa,
     gamma=gamma,
     lr_schedule=[
         learning_rate_for_update(
-            base_learning_rate=1e-1,
+            base_learning_rate=1e-2,
             update_number=step,
             period=100,
             numerator_power=0.51,
         )
-        # ~300k needed to reach optimal policy on 4x4
+        # ~10k and ~100k needed to reach strong and very strong policies
         for step in range(10_000)
     ],
-    eps_schedule=[0.2, 0.1, 0.05, 0.02, 0.01, 0.0],
-    verbose=False
+    eps_schedule=np.linspace(0.5, 0.0, 30),
+    verbose=True,
 )
-print(
-    q.join(policy, on=["state", "action"])
-    .group_by("state")
-    .agg(v=(pl.col("policy") * pl.col("q")).sum())
-    .sort("state")
-)
-print(policy.pivot(on="action", index="state").sort("state"))
+print(q_weights)
 
 # %%
 # Run learned policy
-human_env = gym.make(**env_params, render_mode='human')
-
 num_episodes = 3
+
+human_env = gym.make(**env_params, render_mode='human')
+action_space = range(human_env.action_space.n)
+
 for episode in tqdm(range(num_episodes), desc="Running episodes"):
     state, _ = human_env.reset()
 
     episode_over = False
     while not episode_over:
-        action = sample_from_policy(policy, state)
+        action = sample_from_eps_greedy_policy(
+            state, q _weights, action_space, eps=0.0
+        )
         state, _, terminated, truncated, _ = human_env.step(action)
         episode_over = terminated or truncated
 human_env.close()
